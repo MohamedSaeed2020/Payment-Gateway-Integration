7f5292b605d357789c079f7d4fc0a2c3c8fa77a4
Mohamed Saeed <mhamad.saeed2015.ms@gmail.com>
2022-08-03 23:46:43 +0200

    Delete payments directory

--- payments/lib/shared/components/constants.dart
+++
@@ -1,9 +0,0 @@
-String paymobApiKey = 'ZXlKaGJHY2lPaUpJVXpVeE1pSXNJblI1Y0NJNklrcFhWQ0o5LmV5SmpiR0Z6Y3lJNklrMWxjbU5vWVc1MElpd2ljSEp2Wm1sc1pWOXdheUk2TWpVNE1qTTVMQ0p1WVcxbElqb2lhVzVwZEdsaGJDSjkuWXlOTUJyVk54OHVGSXExQzdiZGpVYnJ1eDBQbU5GMklqc0dqN2FSWS1Cb3F4MXFTSjlhQWVKN1ZLMDlwZ0pBNnZOZlNvRUU4aWVEX0l5amlzY0ptZkE=';
-String paymobAuthenticationToken = '';
-String orderId = '';
-String cardPaymentToken = '';
-String kioskPaymentToken = '';
-String cardIntegrationId = '2498254';
-String kioskIntegrationId = '2498422';
-String referenceCode = '';
-const String apiUrl = 'https://accept.paymob.com/api/';
